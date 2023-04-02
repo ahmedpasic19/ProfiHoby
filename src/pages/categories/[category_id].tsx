332fce6f0555092b7abd4882c762816e4018d343
Ahmed Pašić <ahmedpasic15@gmail.com>
2023-04-02 14:33:03 +0200

    [category_id] page dispays its groups and group_articles

--- src/pages/categories/[category_id].tsx
+++ src/pages/categories/[category_id].tsx
@@ -3,20 +3,40 @@ import { useRouter } from 'next/router'
 import { useEffect, useRef, useState } from 'react'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import { trpcClient } from '../../utils/api'
-import { TArticle } from '../../types/article'
 
 import Article from '../../components/Article'
 import Spinner from '../../components/Spinner'
+import {
+  Article as ArticleModel,
+  ArticleAction,
+  ArticleGroups,
+  CategoriesOnArticle,
+  Category,
+  Group,
+  Image,
+} from '@prisma/client'
 
 type TProps = {
   initialData: {
     pages: {
-      articles: TArticle[]
+      category:
+        | (Category & {
+            groups: (Group & {
+              articles: (ArticleGroups & {
+                article: ArticleModel & {
+                  image: Image[]
+                  categories: (CategoriesOnArticle & { category: Category })[]
+                  action: ArticleAction | null
+                }
+              })[]
+            })[]
+          })
+        | null
       pageIndex: number
       pageCount: number
       pageSize: number
-    }
-    pageParams: null[]
+    }[]
+    pageParams: string[]
   }
 }
 
@@ -29,20 +49,15 @@ const CategoryArticles: NextPage<TProps> = ({ initialData }) => {
   const { data, fetchNextPage, isFetchingNextPage, isSuccess } =
     useInfiniteQuery(
       [category_id],
-      ({ pageParam = 1, queryKey }) => {
-        const [category_id] = queryKey
-        return trpcClient.article.getAllArticleByCategoryID.query({
-          pageSize: 3,
-          pageIndex: pageParam as number,
+      ({ pageParam = 1 }) =>
+        trpcClient.category.getAllCategoryWithGroupsAndArticles.query({
           category_id: typeof category_id === 'string' ? category_id : '',
-          name: '',
-        })
-      },
+          pageIndex: pageParam as number,
+          pageSize: 3,
+        }),
       {
-        // Fetch next page based on prev response
-        getNextPageParam: (data: (typeof initialData)['pages']) =>
+        getNextPageParam: (data) =>
           data.pageIndex === data.pageCount ? undefined : data.pageIndex + 1,
-        //@ts-ignore
         initialData,
       }
     )
@@ -75,21 +90,29 @@ const CategoryArticles: NextPage<TProps> = ({ initialData }) => {
 
   return (
     <div className='flex h-full w-full flex-col px-10 pt-[8vh]'>
-      <div className='grid grid-cols-4 gap-5 pb-20'>
+      <div className='flex flex-col gap-5 pb-20'>
         {isSuccess &&
           data.pages.map((page) =>
-            page.articles.map((article) => (
-              <Article
-                key={article.id}
-                action={article.article_action_id ? true : false}
-                actionPercentage={article?.action?.discount}
-                name={article.name}
-                description={article.description}
-                imageURL={article.image[0]?.url || ''}
-                price={article.base_price}
-                categories={article.categories}
-                article_id={article.id}
-              />
+            page.category?.groups.map((group) => (
+              <div key={Math.random()} className='flex flex-col'>
+                <label>{group.name}</label>
+                <ul className='flex'>
+                  {group.articles.map((article) => (
+                    <Article
+                      key={Math.random()}
+                      action={article.article.article_action_id ? true : false}
+                      actionPercentage={article.article.action?.discount}
+                      name={article.article.name}
+                      categories={article.article.categories}
+                      //@ts-ignore // Error: "url doesn't exits on image", but it does exits
+                      imageURL={(article.article.image[0]?.url as string) || ''}
+                      price={article.article.base_price}
+                      description={article.article.description}
+                      article_id={article.article_id}
+                    />
+                  ))}
+                </ul>
+              </div>
             ))
           )}
       </div>
@@ -116,23 +139,23 @@ export async function getServerSideProps(context: {
 }) {
   const category_id = context.params.category_id
 
-  const res = await trpcClient.article.getAllArticleByCategoryID.query({
-    category_id,
-    pageIndex: 0,
-    name: '',
-    pageSize: 3,
-  })
+  const res =
+    await trpcClient.category.getAllCategoryWithGroupsAndArticles.query({
+      category_id,
+      pageIndex: 0,
+      pageSize: 3,
+    })
 
   const initialData = {
     pages: [
       {
-        articles: res.articles,
+        category: res.category,
         pageIndex: res.pageIndex,
         pageCount: res.pageCount,
         pageSize: res.pageSize,
       },
     ],
-    pageParams: [null],
+    pageParams: [category_id],
   }
 
   return {
--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -2,6 +2,20 @@ import { z } from 'zod'
 
 import { createTRPCRouter, adminProcedure, publicProcedure } from '../trpc'
 
+import AWS from 'aws-sdk'
+import { env } from 'process'
+
+const BUCKET_REGION = env.BUCKET_REGION
+const BUCKET_NAME = env.BUCKET_NAME
+const SECRET_ACCES_KEY = env.SECRET_ACCES_KEY
+const ACCESS_KEY = env.ACCESS_KEY
+
+const s3 = new AWS.S3({
+  region: BUCKET_REGION,
+  accessKeyId: ACCESS_KEY,
+  secretAccessKey: SECRET_ACCES_KEY,
+})
+
 export const categoryRouter = createTRPCRouter({
   createCategory: adminProcedure
     .input(
@@ -65,6 +79,67 @@ export const categoryRouter = createTRPCRouter({
 
     return all_categories
   }),
+
+  getAllCategoryWithGroupsAndArticles: publicProcedure
+    .input(
+      z.object({
+        category_id: z.string(),
+        pageSize: z.number(),
+        pageIndex: z.number(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const category = await ctx.prisma.category.findUnique({
+        include: {
+          groups: {
+            skip: input.pageSize * input.pageIndex,
+            take: input.pageSize,
+            include: {
+              articles: {
+                include: {
+                  article: {
+                    include: {
+                      action: true,
+                      categories: { include: { category: true } },
+                      image: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        where: { id: input.category_id },
+      })
+
+      const total_groups = await ctx.prisma.group.count()
+
+      const pageCount = Math.ceil(total_groups / input.pageSize)
+
+      // Assigning an accessURL from S3
+      if (category) {
+        for (const group of category.groups) {
+          for (const article of group.articles) {
+            const extended_images = article.article.image.map((image) => ({
+              ...image,
+              url: s3.getSignedUrl('getObject', {
+                Bucket: BUCKET_NAME,
+                Key: image.image,
+              }),
+            }))
+
+            article.article.image = extended_images
+          }
+        }
+      }
+
+      return {
+        category: category,
+        pageCount,
+        pageIndex: input.pageIndex,
+        pageSize: input.pageSize,
+      }
+    }),
 
   getCategory: adminProcedure
     .input(z.object({ id: z.string() }))
