56ebd0f4895786ff63d35ba538d44f586218b197
Ahmed Pašić <ahmedpasic15@gmail.com>
2023-04-16 15:06:04 +0200

    Implemented react-dropzone for image uploads

--- src/components/layout/forms/articles/UploadImageForm.tsx
+++ src/components/layout/forms/articles/UploadImageForm.tsx
@@ -1,7 +1,9 @@
-import { useState, ChangeEvent, FormEvent, useEffect } from 'react'
+import { useState, FormEvent, useEffect, useCallback } from 'react'
 import { trpcClient } from '../../../../utils/api'
 import { useRouter } from 'next/router'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+
+import { useDropzone } from 'react-dropzone'
 
 import Image from 'next/image'
 import * as Bs from 'react-icons/bs'
@@ -19,7 +21,7 @@ const UploadImageForm = ({
   navigateBack,
 }: TProps) => {
   const [articleImage, setArticleImage] = useState<string>('')
-  const [file, setFile] = useState<File | undefined>(undefined)
+  const [files, setFiles] = useState<File[] | undefined>(undefined)
 
   const router = useRouter()
 
@@ -43,17 +45,17 @@ const UploadImageForm = ({
     {
       onSuccess: async (data) => {
         setArticleImage('')
-        setFile(undefined)
+        setFiles(undefined)
 
-        if (!file) return alert('No File')
+        if (!files) return alert('No File')
 
         // fileds returned from S3 presigned URL
         const fileds: Record<string, unknown> = { ...data?.fields }
         const url: string | undefined = data?.url
         const fileData: Record<string, unknown> = {
           ...fileds,
-          'Content-Type': file.type,
-          file,
+          'Content-Type': files[0]?.type,
+          file: files[0],
         }
 
         const formData = new FormData()
@@ -88,26 +90,29 @@ const UploadImageForm = ({
     }
   }, [images, setPageIndex, router])
 
-  const onImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.currentTarget.files?.[0]) return
+  // Select image function to state
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // set Files to state
+    setFiles(acceptedFiles)
 
-    const selectedImage = e.currentTarget.files
-    const selectedImageArray = Array.from(selectedImage)
-
+    // Display image when selected
+    const selectedImageArray = Array.from(acceptedFiles)
     const imagesArray = selectedImageArray.map((file) => {
       return URL.createObjectURL(file)
     })
 
-    setFile(e.currentTarget.files?.[0])
     if (!imagesArray[0]) return
+
     setArticleImage(imagesArray[0])
-  }
+  }, [])
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop })
 
   const handleUploadImage = (e: FormEvent<HTMLElement>) => {
     e.preventDefault()
 
     if (
-      !file ||
+      !files ||
       article_id === null ||
       article_id === undefined ||
       action_id === null ||
@@ -118,9 +123,27 @@ const UploadImageForm = ({
     createImage({
       article_id,
       action_id,
-      name: file.name,
+      name: files[0]?.name || '',
     })
   }
+
+  // Display empty slots if array of images.length is < 8
+  const insertPlaceholderImages = (article_images: typeof images) => {
+    if (article_images?.length === 0) {
+      const images = new Array(8).fill('Placeholder image')
+      return images as string[]
+    } else {
+      // Find the max length and images array
+      const diff = 8 - (article_images?.length || 0)
+
+      const filled_images = article_images?.concat(
+        new Array(diff).fill('placeholder image')
+      )
+      return filled_images
+    }
+  }
+
+  const image_slots = insertPlaceholderImages(images)
 
   return (
     <form
@@ -131,19 +154,24 @@ const UploadImageForm = ({
         Dodaj dodaj sliku
       </h1>
 
-      <fieldset>
-        <label>Odaberi sliku</label>
-        <input type='file' onChange={onImageChange} />
+      <fieldset
+        {...getRootProps()}
+        className='mt-2 h-full rounded-xl border-2 border-gray-800 text-lg font-semibold text-gray-800'
+      >
+        <input {...getInputProps()} />
+        <div className='flex h-[300px] w-full cursor-pointer items-center justify-center'>
+          <p>Povuci sliku ili klikni ovdije...</p>
+        </div>
       </fieldset>
 
-      <div className='flex h-[300px] w-full items-center justify-center '>
+      <div className='my-4 flex h-full w-full cursor-pointer items-center justify-center'>
         {/* eslint-disable-next-line */}
         <img src={articleImage} />
       </div>
 
-      <section className='mt-14 flex w-full items-center justify-evenly'>
+      <section className='mt-4 flex w-full items-center justify-evenly'>
         <button
-          disabled={!file}
+          disabled={!files?.length}
           onSubmit={handleUploadImage}
           className='w-4/5 max-w-[200px] rounded-xl bg-gray-800 p-4 text-center text-xl font-semibold text-gray-300 hover:bg-gray-700 disabled:bg-gray-600'
         >
@@ -160,20 +188,29 @@ const UploadImageForm = ({
         )}
       </section>
 
-      <div className='mt-5 grid w-full grid-cols-8 grid-rows-1 gap-2 overflow-x-auto'>
-        {images?.map((image) => (
-          <div
-            key={Math.random().toString()}
-            className='flex h-full w-full items-center justify-center'
-          >
-            <Image
-              alt='article image'
-              src={image.url}
-              width={300}
-              height={300}
-            />
-          </div>
-        ))}
+      <div className='mt-5 grid w-full grid-cols-8 grid-rows-1 gap-1 overflow-x-auto'>
+        {image_slots?.map((image) => {
+          if (typeof image !== 'string')
+            return (
+              <div
+                key={Math.random().toString()}
+                className='flex h-full w-full items-center justify-center'
+              >
+                <Image
+                  alt='article image'
+                  src={image.url}
+                  width={300}
+                  height={300}
+                />
+              </div>
+            )
+          return (
+            <div
+              key={Math.random().toString()}
+              className='mih-h-1 relative z-20 flex h-full w-full animate-pulse items-center justify-center bg-gray-400'
+            ></div>
+          )
+        })}
       </div>
     </form>
   )
