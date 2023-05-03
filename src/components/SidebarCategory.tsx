0f483454c9efe1dadc019ba52730920040b06e34
Ahmed Pašić <ahmedpasic15@gmail.com>
2023-05-03 14:54:32 +0200

    Homepage styles - category & articles

--- src/components/SidebarCategory.tsx
+++ src/components/SidebarCategory.tsx
@@ -14,9 +14,9 @@ const SidebarCategory = ({ id, name, groups }: TProps) => {
   const [openCategory, setOpenCategory] = useState(false)
 
   return (
-    <div className='flex h-fit w-full flex-col items-center justify-center bg-gray-600 pt-2'>
-      <div className='flex w-full items-center pb-2'>
-        <label className='ml-5 w-full text-lg font-semibold text-gray-200'>
+    <div className='relative flex h-fit w-full flex-col items-center justify-center border-b-2 border-white bg-gray-800 pt-2'>
+      <div className=' flex w-full items-center justify-center pb-2'>
+        <label className='flex w-full items-center justify-start pl-4 text-lg font-semibold text-gray-200'>
           <Link
             href={`/categories/${id}`}
             className='underline underline-offset-4'
@@ -26,24 +26,34 @@ const SidebarCategory = ({ id, name, groups }: TProps) => {
         </label>
         <button
           onClick={() => setOpenCategory((prev) => !prev)}
-          className='mb-[2px] mr-2 flex h-full items-center justify-center rounded-xl bg-gray-300 px-2 outline-none hover:bg-gray-400'
+          className='mr-2 flex h-full w-fit items-center justify-center'
         >
-          <AiFillCaretDown className='h-5 w-5 text-gray-50' />
+          <AiFillCaretDown
+            className={`h-5 w-5 ${
+              openCategory ? 'rotate-0' : '-rotate-90'
+            } text-gray-50`}
+          />
         </button>
+        {openCategory && (
+          <ul
+            className={`absolute top-10 z-10 flex w-full flex-col ${
+              openCategory
+                ? 'animate-slide-in-group'
+                : 'animate-slide-out-group'
+            }`}
+          >
+            {groups.map((group) => (
+              <Link
+                key={group.id}
+                href={`/groups/${group.id}`}
+                className='text-md bg-gray-700 py-1 pl-5 text-gray-300 underline underline-offset-4 hover:bg-gray-400'
+              >
+                {group.name}
+              </Link>
+            ))}
+          </ul>
+        )}
       </div>
-      {openCategory && (
-        <ul className='flex w-full flex-col'>
-          {groups.map((group) => (
-            <Link
-              key={group.id}
-              href={`/groups/${group.id}`}
-              className='text-md bg-gray-500 py-1 pl-5 text-gray-300 underline underline-offset-4 hover:bg-gray-400'
-            >
-              {group.name}
-            </Link>
-          ))}
-        </ul>
-      )}
     </div>
   )
 }
