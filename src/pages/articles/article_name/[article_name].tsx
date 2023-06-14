4feacc35b8ec5b0d2269efe7afc4469f0737871d
Ahmed Pašić <ahmedpasic15@gmail.com>
2023-06-14 22:42:05 +0200

    Fixed styles on article_name page

--- src/pages/articles/article_name/[article_name].tsx
+++ src/pages/articles/article_name/[article_name].tsx
@@ -1,53 +1,97 @@
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 import { trpcClient } from '../../../utils/api'
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 
 import Article from '../../../components/Article'
-import PagePagination from '../../../components/layout/PagePagination'
+import Spinner from '../../../components/Spinner'
 
 const ArticleNamePage = () => {
-  const [pageIndex, setPageIndex] = useState(0)
+  const [isVisible, setIsVisible] = useState(false)
 
   const router = useRouter()
   const { article_name } = router.query
 
   const name = typeof article_name === 'string' ? article_name : ''
 
-  const { data: articles } = useQuery(
-    ['articles', { name: article_name }],
-    () =>
-      trpcClient.article.getAllArticles.query({
-        name,
-        pageSize: 25,
-        pageIndex,
-      })
-  )
+  const { data, fetchNextPage, isFetchingNextPage, isSuccess } =
+    useInfiniteQuery(
+      ['articles.getAllArticles'],
+      ({ pageParam = 0 }) =>
+        trpcClient.article.getAllArticles.query({
+          name,
+          pageSize: 12,
+          pageIndex: pageParam as number,
+        }),
+      {
+        getNextPageParam: (data) => {
+          return data.pageIndex === data.pageCount
+            ? undefined
+            : data.pageIndex + 1
+        },
+      }
+    )
+
+  // ref to the div at the bottom of the page
+  const ref = useRef<HTMLDivElement>(null)
+
+  // Check if div at the bottom of the page is in view
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry?.isIntersecting) {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false)
+      }
+    })
+
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+  }, [])
+
+  // Fetch new data if the div in the bottom of the page is in view
+  useEffect(() => {
+    if (isVisible) {
+      // execute the desired function here
+      fetchNextPage().catch(console.error)
+    }
+  }, [isVisible, fetchNextPage])
 
   return (
-    <div>
+    <div className='pt-[12vh]'>
       <div className='flex w-full items-center justify-center'>
-        <div className='grid h-full w-full grid-cols-4 gap-5 px-10 pt-[25vh]'>
-          {articles?.articles?.map((article) => (
-            <Article
-              key={article.id}
-              name={article.name}
-              action={article.article_action_id ? true : false}
-              actionPercentage={article?.action?.discount}
-              imageURL={article.image[0]?.access_url || ''}
-              price={article.base_price}
-              categories={article.categories}
-              article_id={article.id}
-            />
-          ))}
+        <div className='grid w-full grid-cols-2 gap-4 px-3 sm:grid-cols-3 md:gap-2 xl:grid-cols-4 2xl:grid-cols-6 2xl:gap-2 4k:grid-cols-10'>
+          {isSuccess &&
+            data?.pages?.map((page) =>
+              page.articles.map((article) => (
+                <Article
+                  key={article.id}
+                  name={article.name}
+                  action={article.article_action_id ? true : false}
+                  actionPercentage={article?.action?.discount}
+                  imageURL={article.image[0]?.access_url || ''}
+                  price={article.base_price}
+                  categories={article.categories}
+                  article_id={article.id}
+                />
+              ))
+            )}
         </div>
       </div>
-      <PagePagination
-        pageCount={articles?.pageCount || 0}
-        pageIndex={pageIndex}
-        setPageIndex={setPageIndex}
-      />
+
+      {isFetchingNextPage && (
+        <div className='flex w-full items-center justify-center text-center'>
+          <Spinner />
+          Učitavanje...
+        </div>
+      )}
+
+      {/* Fetch more when this div is in view */}
+      <div ref={ref} className='z-[100] text-transparent'>
+        BOTTOM ELEMENT
+      </div>
     </div>
   )
 }
