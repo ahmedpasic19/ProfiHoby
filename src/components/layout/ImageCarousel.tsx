29b7385e443ecf0c1325426bdfc0c9ef796824c5
Ahmed Pašić <ahmedpasic15@gmail.com>
2023-06-21 21:06:32 +0200

    Styled image on [article_id] page

--- src/components/layout/ImageCarousel.tsx
+++ src/components/layout/ImageCarousel.tsx
@@ -14,28 +14,29 @@ const ImageCarousel = ({ images }: { images: string[] }) => {
   }
 
   return (
-    <div className='relative w-full p-10'>
-      <div className='absolute inset-0 flex items-center justify-center'>
-        <div className='relative flex'>
+    <div className='relative flex h-full w-full items-center justify-center p-10'>
+      <div className='flex h-full w-full items-center justify-center'>
+        <div className='relative flex h-full w-full items-center justify-center'>
           <button
             onClick={handlePrev}
             disabled={currentImageIndex === 0}
-            className='absolute bottom-1/2 left-5 text-gray-800 disabled:text-gray-400'
+            className='absolute bottom-1/2 left-0 z-20 text-gray-800 disabled:text-gray-400'
           >
             <Bi.BiSkipPreviousCircle className='h-8 w-8' />
           </button>
           {/* eslint-disable */}
           <Image
             src={images[currentImageIndex]!}
-            width={600}
-            height={600}
+            // width={400}
+            // height={400}
+            fill
             alt='article image'
             className='object-contain'
           />
           <button
             onClick={handleNext}
             disabled={currentImageIndex === images?.length - 1}
-            className='absolute bottom-1/2 right-5 text-gray-800 disabled:text-gray-400'
+            className='absolute bottom-1/2 right-5 z-20 text-gray-800 disabled:text-gray-400'
           >
             <Bi.BiSkipNextCircle className='h-8 w-8' />
           </button>
